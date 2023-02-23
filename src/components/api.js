f11dbe93d8f1cb104ab24d790e3c99991ea4b5f4
Alexander Kraskovskii <megoman290300@gmail.com>
2023-02-23 16:12:32 +0500

    created: class API

--- src/components/api.js
+++ src/components/api.js
@@ -1,85 +1,4 @@
-// import { userId, profileImage, apiConfig } from "./constants";
-
-// import { checkServerResponse } from "./utils.js";
-
-// export function getProfileData() {
-//   return fetch(`${apiConfig.baseUrl}/users/me`, {
-//     headers: apiConfig.headers,
-//   }).then(checkServerResponse);
-// }
-
-// // Апи запрос получения данных карточек и их отрисовки
-
-// export function getCardsData() {
-//   return fetch(`${apiConfig.baseUrl}/cards`, {
-//     headers: apiConfig.headers,
-//   }).then(checkServerResponse);
-// }
-
-// // Апи запрос получения имени и профессии пользователя (и их отрисовка)
-
-// export function uploadProfileData(nameValue, aboutValue) {
-//   return fetch(`${apiConfig.baseUrl}/users/me`, {
-//     method: "PATCH",
-//     headers: apiConfig.headers,
-//     body: JSON.stringify({
-//       name: nameValue,
-//       about: aboutValue,
-//     }),
-//   }).then(checkServerResponse);
-// }
-
-// // Функция отправки новой карточки на сервер и ее отрисовка на сайте
-
-// export function uploadNewCard(nameValue, linkValue) {
-//   return fetch(`${apiConfig.baseUrl}/cards`, {
-//     method: "POST",
-//     headers: apiConfig.headers,
-//     body: JSON.stringify({
-//       name: nameValue,
-//       link: linkValue,
-//     }),
-//   }).then(checkServerResponse);
-// }
-
-// // Функция удаления карточки с сервера
-
-// export function removeCardFromServer(card) {
-//   return fetch(`${apiConfig.baseUrl}/cards/${card.dataset.cardId}`, {
-//     method: "DELETE",
-//     headers: apiConfig.headers,
-//   });
-// }
-
-// // Апи запрос добавления лайка на карточке (и отрисовка на сайте)
-
-// export function addLikeOnServer(cardId) {
-//   return fetch(`${apiConfig.baseUrl}/cards/likes/${cardId}`, {
-//     method: "PUT",
-//     headers: apiConfig.headers,
-//   }).then(checkServerResponse);
-// }
-
-// // Апи запрос удаления лайка на карточке (и отрисовка на сайте)
-
-// export function removeLikeOnServer(cardId) {
-//   return fetch(`${apiConfig.baseUrl}/cards/likes/${cardId}`, {
-//     method: "DELETE",
-//     headers: apiConfig.headers,
-//   }).then(checkServerResponse);
-// }
-
-// // Функция обновления ссылки аватара (и его отрисовка на сайте)
-
-// export function updateAvatarOnServer(url) {
-//   return fetch("https://nomoreparties.co/v1/plus-cohort-19/users/me/avatar", {
-//     method: "PATCH",
-//     headers: apiConfig.headers,
-//     body: JSON.stringify({
-//       avatar: url,
-//     }),
-//   }).then(checkServerResponse);
-// }
+// import { userId, profileImage } from "./constants";
 
 class Api {
   constructor(data) {
@@ -94,14 +13,88 @@ class Api {
     return Promise.reject(res.status);
   }
 
+  // получаем изначальныq массив карточек
   getInitialCards() {
     return fetch(`${this.url}/cards`, {
       headers: this.headers
     })
-    .then(this.checkResponse)
-    .then((data) => {console.log(data)})
+      .then(this.checkResponse)
+      .then((data) => { console.log(data) })
   }
-}
+
+  //получаем данные пользователя
+  getProfileData() {
+    return fetch(`${this.url}/users/me`, {
+      headers: this.headers,
+    })
+      .then(this.checkResponse);
+  }
+
+  // получение инфы пользователя
+  uploadProfileData(nameValue, aboutValue) {
+    return fetch(`${this.url}/users/me`, {
+      method: "PATCH",
+      headers: this.headers,
+      body: JSON.stringify({
+        name: nameValue,
+        about: aboutValue,
+      }),
+    })
+      .then(this.checkResponse);
+  }
+
+  // отправка новой карточки на сервер
+  uploadNewCard(nameValue, linkValue) {
+    return fetch(`${this.url}/cards`, {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify({
+        name: nameValue,
+        link: linkValue,
+      }),
+    })
+      .then(this.checkResponse);
+  }
+
+  // удаление карточки с сервера
+  removeCardFromServer(card) {
+    return fetch(`${this.url}/cards/${card.dataset.cardId}`, {
+      method: "DELETE",
+      headers: this.headers,
+    })
+      .then(this.checkResponse);
+  }
+
+  // добавление лайка на карточке
+  addLikeOnServer(cardId) {
+    return fetch(`${this.url}/cards/likes/${cardId}`, {
+      method: "PUT",
+      headers: this.headers,
+    })
+      .then(this.checkResponse);
+  }
+
+  // удаление лайка на карточке
+  removeLikeOnServer(cardId) {
+    return fetch(`${this.url}/cards/likes/${cardId}`, {
+      method: "DELETE",
+      headers: this.headers,
+    }).then(this.checkResponse);
+  }
+
+  // обновления аватара
+  updateAvatarOnServer(url) {
+    return fetch(`${this.url}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this.headers,
+      body: JSON.stringify({
+        avatar: url,
+      }),
+    })
+      .then(this.checkResponse);
+  }
+
+};
 
 const api = new Api({
   baseUrl: 'https://nomoreparties.co/v1/plus-cohort-19',
@@ -110,24 +103,3 @@ const api = new Api({
     'Content-Type': 'application/json'
   }
 });
-
-api.getInitialCards()
-
-console.log('test')
-  // export function getCardsData(){
-  //   return fetch(`${apiConfig.baseUrl}/cards`, {
-  //     headers: apiConfig.headers,
-  //   })
-  //   .then(checkServerResponse)
-  // }
-
-  // другие методы работы с API
-
-
-// const api = new Api({
-//   baseUrl: 'https://nomoreparties.co/v1/cohort-42',
-//   headers: {
-//     authorization: 'c56e30dc-2883-4270-a59e-b2f7bae969c6',
-//     'Content-Type': 'application/json'
-//   }
-// });
